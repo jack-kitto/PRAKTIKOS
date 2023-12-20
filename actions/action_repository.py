da7b2dc980c6c97b4515176105f4f616807461ab
Jack Kitto <jack.kitto@gmail.com>
2023-12-20 21:04:36 +1100

    update action repo to use action factory and add methods for converting actions to and from yaml

--- actions/action_repository.py
+++ actions/action_repository.py
@@ -1,20 +1,29 @@
+import os
 from typing import List
-
+import yaml
+from actions.action_factory import ActionFactory
 from actions.action import Action
-from core.ui import Menu, Option
+from actions.get_mouse_coords_action import GetMouseCoordinatesAction
+from core.ui import Menu
 
 
 class ActionRepository:
     def __init__(self):
         self.actions = []
-        self.main_menu:Menu = Menu('Select Action', [], self.show_info)
+        self.main_menu: Menu = Menu('Select Action', [], self.show_info)
+        self.storage_path = 'storage/actions'
+        # Create the storage directory if it doesn't exist
+        os.makedirs(self.storage_path, exist_ok=True)
+
     def show_info(self):
         pass
 
     def get_all(self) -> List[Action]:
         return self.actions
 
-    def add_action(self, action: Action) -> None:
+    def add_action(self, action_data: dict) -> None:
+        # Use the ActionFactory to create an Action instance
+        action = ActionFactory.create_action(action_data)
         self.actions.append(action)
 
     def print_all(self):
@@ -27,13 +36,20 @@ class ActionRepository:
                 return action
         return None
 
+    def load_actions_from_yaml(self):
+        for filename in os.listdir(self.storage_path):
+            if filename.endswith('.yaml'):
+                full_path = os.path.join(self.storage_path, filename)
+                with open(full_path, 'r') as file:
+                    data = yaml.safe_load(file)
+                    print('loading action', data.get('actions'))
+                    for action in data.get('actions'):
+                        self.add_action(action)
+
+    def save_action_to_yaml(self, action):
+        filename = os.path.join(self.storage_path, f"{action.id}.yaml")
+        with open(filename, 'w') as file:
+            yaml.dump(action.to_dict(), file)
+
 action_repository = ActionRepository()
-
-
-
-action_repository.add_action(Action("Action 1", "This is action 1", 1))
-action_repository.add_action(Action("Action 2", "This is action 2", 2))
-action_repository.add_action(Action("Action 3", "This is action 3", 3))
-action_repository.add_action(Action("Action 4", "This is action 4", 4))
-action_repository.add_action(Action("Action 5", "This is action 5", 5))
-action_repository.add_action(Action("Action 6", "This is action 6", 6))
+action_repository.load_actions_from_yaml()
